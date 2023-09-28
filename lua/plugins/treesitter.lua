5006589a46e887ef2bfc20d0bda6d5c3f20cd2e7
Alejandro Valenzuela <tzalejo@gmail.com>
2023-09-27 22:30:32 -0300

    refactorizo treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 -- treesitter configuration
-return {
+--[[ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
@@ -236,4 +236,95 @@ return {
             require("neogen").setup { snippet_engine = "luasnip" }
         end,
     },
+} ]]
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    event = "VeryLazy",
+    main = "nvim-treesitter.configs",
+    opts = {
+        ensure_installed = {
+            "bash",
+            "blade",
+            "css",
+            "dockerfile",
+            "go",
+            "gomod",
+            "gitignore",
+            "html",
+            "javascript",
+            "json",
+            "graphql",
+            "lua",
+            "luadoc",
+            "markdown",
+            "markdown_inline",
+            "nix",
+            "org",
+            "php",
+            "phpdoc",
+            "query",
+            "rust",
+            "sql",
+            "svelte",
+            "typescript",
+            "tsx",
+            "regex",
+            "vim",
+            "yaml",
+
+        },
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = { "org" },
+        },
+        indent = {
+            enable = true,
+        },
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true,
+                keymaps = {
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@conditional.outer",
+                    ["ic"] = "@conditional.inner",
+                    ["al"] = "@loop.outer",
+                    ["il"] = "@loop.inner",
+                },
+            },
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "gnn",
+                node_incremental = "grn",
+                scope_incremental = "grc",
+                node_decremental = "grm",
+            },
+        },
+    },
+    config = function(plug, config)
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        parser_config.blade = {
+            install_info = {
+                url = "https://github.com/EmranMR/tree-sitter-blade",
+                files = { "src/parser.c" },
+                branch = "main",
+            },
+            filetype = "blade",
+        }
+
+        vim.filetype.add {
+            pattern = {
+                [".*%.blade%.php"] = "blade",
+            },
+        }
+
+        require(plug.main).setup(config)
+    end,
 }
