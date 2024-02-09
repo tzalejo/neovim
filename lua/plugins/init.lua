5718410afb86ff01f8d0492567e094905cad6442
Alejandro Valenzuela <tzalejo@gmail.com>
2024-02-09 12:00:38 -0300

    refactor

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,10 @@
 return {
 
     { "bluz71/vim-nightfly-guicolors" }, -- preferred colorscheme
-
     { "christoomey/vim-tmux-navigator" }, -- tmux & split window navigation
-
     { "szw/vim-maximizer" }, -- maximizes and restores current window
 
     -- essential plugins
-    { "tpope/vim-surround" }, -- add, delete, change surroundings {it's awesome)
     { "inkarkat/vim-ReplaceWithRegister" }, -- replace with register contents using motion {gr + motion)
 
     -- commenting with gc
@@ -17,7 +14,6 @@ return {
     { "b3nj5m1n/kommentary" },
 
     { "rafamadriz/friendly-snippets" }, -- ful snippets
-
     { "jose-elias-alvarez/typescript.nvim" }, -- additional functionality for typescript server {e.g. rename file & update imports)
     { "onsails/lspkind.nvim" }, -- vs-code like icons for autocompletion
 
@@ -27,24 +23,11 @@ return {
     -- Permite seleccion rapida
     { "mg979/vim-visual-multi", branch = "master" },
 
+    { "tpope/vim-dispatch", cmd = { "Make", "Dispatch" } },
+    { "tpope/vim-surround" }, -- add, delete, change surroundings {it's awesome)
     { "tpope/vim-surround" },
     { "tpope/vim-repeat" },
-    { "tpope/vim-dispatch" },
     { "tpope/vim-projectionist" },
 
-    { "noahfrederick/vim-composer" },
-    { "noahfrederick/vim-laravel" },
-    { "Kibadda/laravel-docs.nvim" },
-
-    --[[ {'hrsh7th/cmp-vsnip'},
-    {'hrsh7th/vim-vsnip'},
-    {'hrsh7th/vim-vsnip-integ'},
-    {'saadparwaiz1/cmp_luasnip'}, -- Snippets source for nvim-cmp
-    {'L3MON4D3/LuaSnip'}, -- Snippets plugin
-    {"rafamadriz/friendly-snippets"}, ]]
-
-    { "onecentlin/laravel-blade-snippets-vscode" },
-    { "onecentlin/laravel5-snippets-vscode" },
-
-    { "jwalton512/vim-blade" },
+    { "jwalton512/vim-blade", enable = false },
 }
