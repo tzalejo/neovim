0c0c0c38052137c0347e88772d2df7234250467b
Alejandro Valenzuela <tzalejo@gmail.com>
2024-02-13 19:38:47 -0300

    refactor

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,11 @@
 return {
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
     "lewis6991/gitsigns.nvim",
+    event = "VeryLazy",
     opts = {
+        signcolumn = false,
+        numhl = true,
+        trouble = false,
         -- See `:help gitsigns.txt`
         signs = {
             add = { text = "+" },
