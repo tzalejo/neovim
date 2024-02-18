064c215e5d48a8c796f971b8f683880b9f442777
Alejandro Valenzuela <tzalejo@gmail.com>
2024-02-18 01:03:56 -0300

    cambio tema

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,19 @@
-return {
+--[[ return {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false,
     priority = 1000,
     config = function()
         vim.cmd.colorscheme "catppuccin-mocha"
+} ]]
+return {
+    "wuelnerdotexe/vim-enfocado",
+    lazy = false,
+    priority = 1000,
+    init = function()
+        vim.g.enfocado_style = "neon"
+    end,
+    config = function()
+        vim.cmd.colorscheme "enfocado"
     end,
 }
