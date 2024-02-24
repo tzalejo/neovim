f1f11b80c089c99f1ceaa167095e38690a8e3188
Alejandro Valenzuela <tzalejo@gmail.com>
2024-02-24 10:30:34 -0300

    se cambio algunas configuraciones

--- lua/alejandro/lsp/mason.lua
+++ lua/alejandro/lsp/mason.lua
@@ -1,31 +1,3 @@
---[[ local mason = require "mason"
-local mason_lspconfig = require "mason-lspconfig"
-local servers = require "alejandro.lsp.servers"
-local lspconfig = require "lspconfig"
-
-mason.setup {
-    PATH = "prepend",
-}
-
-local mason_servers = {}
-
---can be improve checking the setup function to see if sets the cmd
-for server, _ in pairs(servers) do
-    local cmd = lspconfig[server].document_config.default_config.cmd[1]
-    if vim.fn.executable(cmd) == 0 then
-        table.insert(mason_servers, server)
-    end
-end
-
-mason_lspconfig.setup {
-    ensure_installed = mason_servers,
-}
-
--- filter the list for the ones not globally installed
-require("mason-tool-installer").setup {
-    ensure_installed = require "alejandro.lsp.tools",
-} ]]
-
 local mason_lspconfig = require "mason-lspconfig"
 local servers = require "alejandro.lsp.servers"
 local lspconfig = require "lspconfig"
@@ -40,12 +12,12 @@ for server, _ in pairs(servers) do
 end
 
 mason_lspconfig.setup {
-    ensure_installed = mason_servers,
+    -- ensure_installed = mason_servers,
 }
 
 -- filter the list for the ones not globally installed
-require("mason-tool-installer").setup {
+--[[ require("mason-tool-installer").setup {
     ensure_installed = vim.tbl_filter(function(tool)
         return vim.fn.executable(tool) == 0
     end, require "alejandro.lsp.tools"),
-}
+} ]]
