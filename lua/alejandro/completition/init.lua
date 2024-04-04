86e1488356aa367bf1c1f09c04dbaa4808ec503e
Alejandro Valenzuela <tzalejo@gmail.com>
2024-04-04 17:31:32 -0300

    configuracion para las ventanas de sugerencia

--- lua/alejandro/completition/init.lua
+++ lua/alejandro/completition/init.lua
@@ -4,8 +4,69 @@ local luasnip = require "luasnip"
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
 local compare = require "cmp.config.compare"
 
-
 cmp.setup {
+    window = {
+        completion = {
+            border = "rounded",
+        },
+        documentation = {
+            border = "rounded",
+        },
+    },
+    formatting = {
+        format = function(entry, vim_item)
+            local KIND_ICONS = {
+                Tailwind = "󰹞󰹞󰹞󰹞󰹞󰹞󰹞󰹞",
+                Color = " ",
+                -- Class = 7,
+                -- Constant = '󰚞',
+                -- Constructor = 4,
+                -- Enum = 13,
+                -- EnumMember = 20,
+                -- Event = 23,
+                -- Field = 5,
+                -- File = 17,
+                -- Folder = 19,
+                -- Function = 3,
+                -- Interface = 8,
+                -- Keyword = 14,
+                -- Method = 2,
+                -- Module = 9,
+                -- Operator = 24,
+                -- Property = 10,
+                -- Reference = 18,
+                Snippet = " ",
+                -- Struct = 22,
+                -- Text = "",
+                -- TypeParameter = 25,
+                -- Unit = 11,
+                -- Value = 12,
+                -- Variable = 6
+            }
+            if vim_item.kind == "Color" and entry.completion_item.documentation then
+                local _, _, r, g, b =
+                    ---@diagnostic disable-next-line: param-type-mismatch
+                    string.find(entry.completion_item.documentation, "^rgb%((%d+), (%d+), (%d+)")
+
+                if r and g and b then
+                    local color = string.format("%02x", r) .. string.format("%02x", g) .. string.format("%02x", b)
+                    local group = "Tw_" .. color
+
+                    if vim.api.nvim_call_function("hlID", { group }) < 1 then
+                        vim.api.nvim_command("highlight" .. " " .. group .. " " .. "guifg=#" .. color)
+                    end
+
+                    vim_item.kind = KIND_ICONS.Tailwind
+                    vim_item.kind_hl_group = group
+
+                    return vim_item
+                end
+            end
+
+            vim_item.kind = KIND_ICONS[vim_item.kind] or vim_item.kind
+            return vim_item
+        end,
+    },
     mapping = {
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -64,21 +125,20 @@ cmp.setup {
         end,
     },
 
-    formatting = {
+    --[[ formatting = {
         format = lspkind.cmp_format {
             with_text = true,
-            menu      = {
-                buffer                    = "[buf]",
-                nvim_lsp                  = "[ ]",
-                nvim_lua                  = "[api]",
-                path                      = "[path]",
-                luasnip                   = "[snip]",
-                copilot                   = "[ﮧ ]",
+            menu = {
+                buffer = "[buf]",
+                nvim_lsp = "[ ]",
+                nvim_lua = "[api]",
+                path = "[path]",
+                luasnip = "[snip]",
+                copilot = "[ﮧ ]",
                 ["vim-dadbod-completion"] = "[DB]",
-
             },
         },
-    },
+    }, ]]
 
     sorting = {
         priority_weight = 2,
