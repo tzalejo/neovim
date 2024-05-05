1c5c174cc13f190dd44201682fddd975f90fa043
Alejandro Valenzuela <tzalejo@gmail.com>
2024-05-05 19:01:44 -0300

    configuracion para snippetsç

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,32 +1,49 @@
 local luasnip = require "luasnip"
 
 return { -- override nvim-cmp plugin
-  "hrsh7th/nvim-cmp",
-  -- override the options table that is used in the `require("cmp").setup()` call
-  opts = function(_, opts)
-    -- opts parameter is the default options table
-    -- the function is lazy loaded so cmp is able to be required
-    local cmp = require "cmp"
-    -- modify the mapping part of the table
-    opts.mapping["<C-x>"] = cmp.mapping.select_next_item()
-    opts.mapping["<C-d>"] = cmp.mapping.scroll_docs(-4)
-    opts.mapping["<C-f>"] = cmp.mapping.scroll_docs(4)
-    opts.mapping["<C-n>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.choice_active() then
-        luasnip.change_choice(1)
-      else
-        fallback()
-      end
-    end, { "i", "s" })
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- require("luasnip.loaders.from_vscode").lazy_load {}
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    -- override the options table that is used in the `require("cmp").setup()` call
+    opts = function(_, opts)
+      -- opts parameter is the default options table
+      -- the function is lazy loaded so cmp is able to be required
+      local cmp = require "cmp"
+      require("luasnip.loaders.from_vscode").lazy_load {}
+      -- modify the mapping part of the table
+      opts.mapping["<C-x>"] = cmp.mapping.select_next_item()
+      opts.mapping["<C-d>"] = cmp.mapping.scroll_docs(-4)
+      opts.mapping["<C-f>"] = cmp.mapping.scroll_docs(4)
+      opts.mapping["<C-n>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.choice_active() then
+          luasnip.change_choice(1)
+        else
+          fallback()
+        end
+      end, { "i", "s" })
 
-    opts.sources = cmp.config.sources {
-      { name = "nvim_lsp", priority = 1000 },
-      { name = "luasnip", priority = 750 },
-      { name = "buffer", priority = 500 },
-      { name = "path", priority = 250 },
-      { name = "emoji", priority = 700 }, -- add new source
-    }
-  end,
+      opts.sources = cmp.config.sources {
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip", priority = 750 },
+        { name = "buffer", priority = 500 },
+        { name = "path", priority = 250 },
+        { name = "emoji", priority = 700 }, -- add new source
+      }
+    end,
+  },
 }
