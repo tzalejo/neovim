a7a54c331bf6a955e20ba32856e7b28a4a652c3f
Alejandro Valenzuela <tzalejo@gmail.com>
2025-05-07 15:31:28 -0300

    fix en la configuracion de tree

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,36 +5,8 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  main = "nvim-treesitter.configs",
-  dependencies = {
-    { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
-  },
-  event = "VeryLazy",
   opts = function(_, opts)
-    local astrocore = require "astrocore"
-    if astrocore.is_available "mason.nvim" then require("lazy").load { plugins = { "mason.nvim" } } end
-    opts = astrocore.extend_tbl(opts, {
-      auto_install = vim.fn.executable "tree-sitter" == 1, -- only enable auto install if `tree-sitter` cli is installed
-      highlight = { enable = true },
-      incremental_selection = { enable = true },
-      indent = { enable = true },
-      -- textobjects = {
-      --   select = {
-      --     enable = true,
-      --     lookahead = true,
-      --     keymaps = {
-      --       ["af"] = { query = "@function.outer", desc = "around function " },
-      --       ["if"] = { query = "@function.inner", desc = "inside function " },
-      --       ["ac"] = { query = "@conditional.outer", desc = "around conditional" },
-      --       ["ic"] = { query = "@conditional.inner", desc = "inside conditional" },
-      --       ["al"] = { query = "@block.outer", desc = "around block" },
-      --       ["il"] = { query = "@block.inner", desc = "inside block" },
-      --     },
-      --   },
-      -- },
-    })
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = astrocore.list_insert_unique(opts.ensure_installed, {
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "javascript",
       "typescript",
       "bash",
@@ -46,6 +18,12 @@ return {
       "css",
       "php",
       "phpdoc",
+      "json",
+      "gitignore",
+      "lua",
+      "sql",
+      "dockerfile",
+      "html",
     })
   end,
   config = function(_, opts)
