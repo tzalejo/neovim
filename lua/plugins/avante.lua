767afaeb614f7dd4aa263ca993d944a473468c2e
Alejandro Valenzuela <tzalejo@gmail.com>
2025-06-02 17:20:14 -0300

    inhabilito para que no me de sugerencias

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -19,7 +19,7 @@ return {
         },
       },
       behaviour = {
-        auto_suggestions = true, -- Experimental stage
+        auto_suggestions = false, -- Experimental stage
         auto_set_highlight_group = true,
         auto_set_keymaps = true,
         auto_apply_diff_after_generation = false,
