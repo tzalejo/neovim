482295a2bde63ae98f84553995fcb883302b0256
Alejandro Valenzuela <tzalejo@gmail.com>
2026-02-23 16:23:43 -0300

    comente porque me generaba error

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -88,23 +88,23 @@ return {
     -- mappings to be set up on attaching of a language server
     mappings = {
       n = {
-        gl = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" },
+        -- gl = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" },
         -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
-        gd = {
-          function() vim.lsp.buf.declaration() end,
-          desc = "Declaration of current symbol",
-          cond = "textDocument/declaration",
-        },
-        gi = {
-          function() vim.lsp.buf.implementation() end,
-          desc = "Implementation of current symbol",
-          cond = "textDocument/declaration",
-        },
-        gr = {
-          function() vim.lsp.buf.references() end,
-          desc = "References of current symbol",
-          cond = "textDocument/declaration",
-        },
+        -- gd = {
+        --   function() vim.lsp.buf.declaration() end,
+        --   desc = "Declaration of current symbol",
+        --   cond = "textDocument/declaration",
+        -- },
+        -- gi = {
+        --   function() vim.lsp.buf.implementation() end,
+        --   desc = "Implementation of current symbol",
+        --   cond = "textDocument/declaration",
+        -- },
+        -- gr = {
+        --   function() vim.lsp.buf.references() end,
+        --   desc = "References of current symbol",
+        --   cond = "textDocument/declaration",
+        -- },
         -- ["<Leader>uy"] = {
         --    function() require("astrolsp.toggles").buffer_semantic_tokens() end,
         --    desc = "Toggle LSP semantic highlight (buffer)",
