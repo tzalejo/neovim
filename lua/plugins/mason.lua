ce6cc46b8dcb5ea4648ef091dfaf9fb8eca5a47a
Alejandro Valenzuela <tzalejo@gmail.com>
2026-03-12 15:53:36 -0300

    fix docker language server

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,6 +9,10 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = function(_, opts)
+      opts.ensure_installed = vim.tbl_filter(
+        function(s) return s ~= "docker-language-server" end,
+        opts.ensure_installed or {}
+      )
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         -- add more arguments for adding more language servers
@@ -17,6 +21,7 @@ return {
         "phpactor",
         -- "stimulus_ls",
         "tailwindcss",
+        "dockerls",
       })
     end,
   },
